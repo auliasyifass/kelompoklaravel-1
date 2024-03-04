798d2ea9cbb8badf041b2a14bf540d0848e09d9c
belajarlaravel <awaliakhoirunnisac@gmail.com>
2024-03-04 15:54:03 +0700

    perbaikan file

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,14 +11,7 @@
 
     <title>SB Admin 2 - Dashboard</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset ('template/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{ asset ('template/css/sb-admin-2.min.css') }}" rel="stylesheet">
+ @include('includes.style')
 
 </head>
 
@@ -28,9 +21,10 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-       
+       @include('includes.sidebar')
         <!-- End of Sidebar -->
-        @include('includes.sidebar')
+
+      
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
 
